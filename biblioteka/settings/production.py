885105e16e6dd5ef71f898599445da09b7fdd07f
Oleg <oleg1248163264@gmail.com>
2020-04-28 18:27:31 +0300

    tweaked settings for production use, tweaked manage.py for local use, tweaked booster for the use with cron

--- biblioteka/settings/production.py
+++ biblioteka/settings/production.py
@@ -3,3 +3,16 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Wwnp2LG8NMtL0ocWQL81',
+        'HOST': 'database-1.cmeyyraaskkp.us-west-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
+
+STATIC_ROOT = '/var/www/biblioteka/static'
--- booster.py
+++ booster.py
@@ -7,26 +7,24 @@ import requests
 from asgiref.sync import async_to_sync
 from bs4 import BeautifulSoup
 from channels.layers import get_channel_layer
+from datetime import datetime
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'biblioteka.settings.base')
 channel_layer = get_channel_layer()
 
-while True:
-    response = requests.get('https://www.livelib.ru/books/top')
-    response.encoding = 'utf-8'
-    html = response.text
-    soup = BeautifulSoup(html, 'html.parser')
-    books = soup.findAll('div', class_='blist-biglist')[0].contents
-    books_string = '; '.join((book.findAll('div', class_='brow-topno')[0].contents[0] +
-                              ' ' +
-                              book.findAll('a', class_='brow-book-name')[0].string for book in books[:5]))
+response = requests.get('https://www.livelib.ru/books/top')
+response.encoding = 'utf-8'
+html = response.text
+soup = BeautifulSoup(html, 'html.parser')
+books = soup.findAll('div', class_='blist-biglist')[0].contents
+books_string = '; '.join((book.findAll('div', class_='brow-topno')[0].contents[0] +
+                      ' ' +
+                      book.findAll('a', class_='brow-book-name')[0].string for book in books[:5]))
 
-    async_to_sync(channel_layer.group_send)(
-        'chat_default',
-        {
-            'type': 'chat_message',
-            'message': books_string,
-        },
-    )
-
-    time.sleep(5)
+async_to_sync(channel_layer.group_send)(
+    'chat_default',
+    {
+        'type': 'chat_message',
+        'message': '{} {}'.format(datetime.now().ctime(), books_string),
+    },
+)
