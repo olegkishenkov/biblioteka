545ba307f9694e3be8c5eda7cce66854f0c5150e
Oleg <oleg1248163264@gmail.com>
2020-05-03 11:03:56 +0300

    removed credentials from production settings

--- biblioteka/settings/production.py
+++ biblioteka/settings/production.py
@@ -8,10 +8,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'Wwnp2LG8NMtL0ocWQL81',
-        'HOST': 'database-1.cmeyyraaskkp.us-west-2.rds.amazonaws.com',
-        'PORT': '5432',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
