b2aa9fc44fe986c1b52527a8753be5343f5dd6b9
Oleg <oleg1248163264@gmail.com>
2020-05-05 01:48:46 +0300

    connected output to stdout with an optional argument --verbose in booster

--- exlibris/management/commands/booster.py
+++ exlibris/management/commands/booster.py
@@ -11,7 +11,7 @@ class Command(BaseCommand):
     help = 'parses booklib.ru book top and sends the first 5 books\' names to the channel layer group chat_default'
 
     def add_arguments(self, parser):
-        pass
+        parser.add_argument('--verbose', '-w', action='store_true', help='print data to stdout')
 
     def handle(self, *args, **options):
         response = requests.get('https://www.livelib.ru/books/top')
@@ -20,16 +20,17 @@ class Command(BaseCommand):
 
         soup = BeautifulSoup(html, 'html.parser')
         books = soup.findAll('div', class_='blist-biglist')[0].contents
-        books_string = '; '.join((book.findAll('div', class_='brow-topno')[0].contents[0] +
+        books_string = '\n'.join((book.findAll('div', class_='brow-topno')[0].contents[0] +
                                   ' ' +
-                                  book.findAll('a', class_='brow-book-name')[0].string for book in books[:5]))
+                                  book.findAll('a', class_='brow-book-name')[0].string for book in books[:100]))
         datetime_string = datetime.now().ctime()
-
+        if options['verbose']:
+            self.stdout.write(datetime_string+'\n'+books_string)
         channel_layer = get_channel_layer()
         async_to_sync(channel_layer.group_send)(
             'chat_default',
             {
                 'type': 'chat_message',
-                'message': '{} {}'.format(datetime_string, books_string),
+                'message': '{}\n{}'.format(datetime_string, books_string),
             },
         )
