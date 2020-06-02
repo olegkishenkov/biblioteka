6e3f150eafed152d883b7005c7173b3033bb7d92
oleg1248 <oleg1248163264@gmail.com>
2020-06-02 18:54:30 +0300

    tweaked the projects so that it can be run in 3 containers

--- biblioteka/settings/local.py
+++ biblioteka/settings/local.py
@@ -3,3 +3,14 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'test123',
+        'HOST': 'db',
+        'POSRT': '5432'
+    }
+}
